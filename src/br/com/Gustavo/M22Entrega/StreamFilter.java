f93cc7d5073993962b30fdabd1948b7fba789637
Gustavo Dias <gustavogdcosta@hotmail.com>
2024-07-29 14:28:56 -0300

    primeiro commit

---
+++ src/br/com/Gustavo/M22Entrega/StreamFilter.java
@@ -0,0 +1,18 @@
+package br.com.Gustavo.M22Entrega;
+
+import java.util.List;
+
+public class StreamFilter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		List<Pessoa> lista =  new Pessoa().Info();
+		
+		lista.stream()
+				.filter(pessoa -> pessoa.getSexo().equals("feminino"))
+				.forEach(p -> System.out.println(p.getNome()));
+				
+	}
+
+}
